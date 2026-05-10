66fb0e3a3eccbb64dbcc1d6a4dfc067ad8e59016
Keith Timko <kbtimko@gmail.com>
2026-05-09 22:55:09 -0400

    fix: guard against null target in Projectile constructor

--- src/entities/Projectile.js
+++ src/entities/Projectile.js
@@ -2,9 +2,9 @@ export class Projectile {
   constructor({ x, y, target, damage, splashRadius = 0, pierce = false, slowFactor = 0, color = 0xffffff }) {
     this.x = x;
     this.y = y;
-    this.target = target;             // Enemy instance (homing) or null (AoE)
-    this.targetX = target.x;
-    this.targetY = target.y;
+    this.target = target;
+    this.targetX = target ? target.x : x;
+    this.targetY = target ? target.y : y;
     this.damage = damage;
     this.splashRadius = splashRadius;
     this.pierce = pierce;
